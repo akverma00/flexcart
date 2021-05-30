3db8f104a6d9101757aaa0d941b4e4a30b1f7f69
Arun Kumar Verma <arunkumarverma22022000@gmail.com>
2021-05-30 14:05:18 +0530

    Added 2 components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,31 @@
-import React from 'react';
-import data from './data';
-import Product from './components/Product'
+import React from "react";
+import data from "./data";
+import Product from "./components/Product";
 
 function App() {
-    return (
-        <div className="grid-container">
-            <header className="row">
-                <div>
-                    <a href="/" className="brand">FlexCart</a>
-                </div>
-                <div>
-                    <a href="/cart" >Cart</a>
-                    <a href="/signin" >Signin</a>
-                </div>
-
-            </header>
-            <main>
-                <div className="row center">
-                    {
-                        data.products.map((product) => (
-                            <Product key={product._id} product={product} />
-                        ))
-                    }
-                </div>
-            </main>
-            <footer className="row center">
-                Designed by Arun
-            </footer>
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a href="/" className="brand">
+            FlexCart
+          </a>
         </div>
-    );
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Signin</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      </main>
+      <footer className="row center">Designed by Arun</footer>
+    </div>
+  );
 }
 
 export default App;
