808e577ce4cec4a9a8dbd706bc371a5b0580e02a
Arun Kumar Verma <arunkumarverma22022000@gmail.com>
2021-05-30 12:49:19 +0530

    Initialized React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div class="grid-container">
+            <header class="row">
+                <div>
+                    <a href="/" class="brand">FlexCart</a>
+                </div>
+                <div>
+                    <a href="/cart" >Cart</a>
+                    <a href="/signin" >Signin</a>
+                </div>
+
+            </header>
+            <main>
+                <div class="row center">
+                    {
+                        data.products.map((product) => {
+                            return(    
+                                <div key={product._id} class="card" >
+                                    <a href={`/product/${product._id}`}>
+                                        <img class="medium" src={product.image} alt={product.name} />
+                                    </a>
+                                    
+                                    <div class="card-body">
+                                        <a href={`/product/${product._id}`}>
+                                            <h2>{product.name}</h2>
+                                        </a>
+                                        <div class="rating">
+                                            <span><i class="fa fa-star" ></i></span>
+                                            <span><i class="fa fa-star" ></i></span>
+                                            <span><i class="fa fa-star" ></i></span>
+                                            <span><i class="fa fa-star" ></i></span>
+                                            <span><i class="fa fa-star" ></i></span>
+                                        </div>
+                                        <div class="price">
+                                            ${product.price}
+                                        </div>
+                                    </div>
+                                </div>
+                            );                        
+                        })
+                    }
+                </div>
+            </main>
+            <footer class="row center">
+                Designed by Arun
+            </footer>
+        </div>
+    );
 }
 
 export default App;
