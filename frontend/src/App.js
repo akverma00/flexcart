bf8b3cec27526e1b1d4628a95934d8794321faf7
Arun Kumar Verma <arunkumarverma22022000@gmail.com>
2021-05-30 17:28:24 +0530

    Product page is created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,32 @@
 import React from "react";
-import data from "./data";
-import Product from "./components/Product";
+import { BrowserRouter, Route } from "react-router-dom"
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a href="/" className="brand">
-            FlexCart
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a href="/" className="brand">
+              FlexCart
           </a>
-        </div>
-        <div>
-          <a href="/cart"> Cart </a>
-          <a href="/signin"> Signin </a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product} />
-          ))}
-        </div>
-      </main>
-      <footer className="row center">Designed by Arun</footer>
-    </div>
+          </div>
+          <div>
+            <a href="/cart"> Cart </a>
+            <a href="/signin"> Signin </a>
+          </div>
+        </header>
+        <main>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </main>
+        <footer className="row center">Designed by Arun</footer>
+      </div>
+
+    </BrowserRouter>
   );
 }
 
